0faa92fb2e5dccc3324f86e234676a3c45c328d7
Bahram Movlanov <movlan@users.noreply.github.com>
2020-08-24 23:36:46 -0600

    done lecture 78

---
+++ code/06_sort/build/index.js
@@ -0,0 +1,22 @@
+var Sorter = (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([1, 6, -1, 0]);
+sorter.sort();
+console.log(sorter.collection);
+//# sourceMappingURL=index.js.map
