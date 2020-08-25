7097fcd77e6a13f5136f93d2c9599a3a2f551005
Bahram Movlanov <movlan@users.noreply.github.com>
2020-08-24 21:30:05 -0600

    end of section 9

--- code/05_maps/src/Company.ts
+++ code/05_maps/src/Company.ts
@@ -16,4 +16,13 @@ export class Company {
       lng: parseFloat(faker.address.longitude()),
     };
   }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h2>Company Name: ${this.companyName}</h2>
+        <h3>Catchphrase: ${this.catchPhrase}</h3>
+      </div>
+    `;
+  }
 }
---
+++ code/05_maps/src/CustomMap.ts
@@ -0,0 +1,37 @@
+// instructions to every other class
+// on how they can be an arguments to "addMarker"
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
