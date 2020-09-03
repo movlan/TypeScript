ff95e1c7fad18685bb12222077557a3cb31b93e8
Bahram Movlanov <movlan@users.noreply.github.com>
2020-09-03 11:28:41 -0600

    end of video 84

--- code/06_sort/src/index.ts
+++ code/06_sort/src/index.ts
@@ -1,30 +1,10 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
+const numbersCollection = new NumbersCollection([10, 3, -5, 12, 0]);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // if collection is an array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        //If collection is string, do this logic isnstead
-        if (typeof this.collection === "string") {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([1, 6, -1, 0]);
+const sorter = new Sorter(numbersCollection);
 
 sorter.sort();
 
-console.log(sorter.collection);
+console.log(numbersCollection.data);
