5810937b9504464a3956859c20ebc2db0f656750
Bahram Movlanov <movlan@users.noreply.github.com>
2020-09-03 11:37:53 -0600

    end of 85

--- code/06_sort/src/index.ts
+++ code/06_sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 12, 0]);
+const numbersCollection = new NumbersCollection([100, 3, -5, 12, 0]);
 
 const sorter = new Sorter(numbersCollection);
 
