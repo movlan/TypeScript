7e918fd9afa23925a13b727d4a3938fc5cd978ff
Bahram Movlanov <movlan@users.noreply.github.com>
2020-09-10 11:01:07 -0600

    end of lecture 152 before User refractor

---
+++ code/08_web/src/index.ts
@@ -0,0 +1,5 @@
+import { User } from "./models/User";
+
+const user = new User({ name: "New User", age: 0 });
+
+user.save();
---
+++ code/08_web/src/models/User.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosResponse } from "axios";
+
+interface UserProps {
+  name?: string;
+  age?: number;
+  id?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data);
+    }
+  }
+}
