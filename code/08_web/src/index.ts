a499e96bf0e06ccaab869995de0c6c4868264c89
Bahram Movlanov <movlan@users.noreply.github.com>
2020-09-15 12:10:18 -0600

    End of lecture 180

--- code/08_web/src/index.ts
+++ code/08_web/src/index.ts
@@ -1,6 +1,6 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = User.buildUser({ id: 1 });
 
 user.on("change", () => {
   console.log(user);
--- code/08_web/src/models/User.ts
+++ code/08_web/src/models/User.ts
@@ -1,7 +1,7 @@
+import { Model } from "./Model";
 import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { ApiSync } from "./ApiSync";
 import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
 
 export interface UserProps {
   name?: string;
@@ -11,49 +11,12 @@ export interface UserProps {
 
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.attributes.get("id");
-
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an id");
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger("save");
-      });
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 }
