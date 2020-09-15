6986219ab44486ba0a45b4baefdd2a3425d06d4d
Bahram Movlanov <movlan@users.noreply.github.com>
2020-09-15 13:19:02 -0600

    End of lecture 184

--- code/08_web/src/index.ts
+++ code/08_web/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = User.buildUser({ id: 1 });
+const collection = User.buildUserCollection();
 
-user.on("change", () => {
-  console.log(user);
+collection.on("change", () => {
+  console.log(collection);
 });
 
-user.fetch();
+collection.fetch();
--- code/08_web/src/models/User.ts
+++ code/08_web/src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from "./Model";
 import { Eventing } from "./Eventing";
 import { ApiSync } from "./ApiSync";
 import { Attributes } from "./Attributes";
+import { Collection } from "./Collection";
 
 export interface UserProps {
   name?: string;
@@ -18,5 +19,10 @@ export class User extends Model<UserProps> {
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
     );
+  }
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) => {
+      return User.buildUser(json);
+    });
   }
 }
