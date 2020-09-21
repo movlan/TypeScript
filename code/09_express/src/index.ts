b37b91115fc9c3142441e80b88d3308e7c032c1f
Bahram Movlanov <movlan@users.noreply.github.com>
2020-09-21 11:27:12 -0600

    end of lecture 212

---
+++ code/09_express/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi There!</h1>
+    </div>
+   `);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
